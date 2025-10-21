4e229a55c3102b2b158f228190d0aa7378dc7dbb
Benjamin Rosales <144568456+benjarxv@users.noreply.github.com>
2025-10-21 13:10:39 -0300

    Fix Usuario vs User en Auth
    Lo que dice arriba

--- app/src/main/java/com/example/vidasalud/repository/AuthRepository.kt
+++ app/src/main/java/com/example/vidasalud/repository/AuthRepository.kt
@@ -1,59 +1,53 @@
 package com.example.vidasalud.repository
 
-import com.example.vidasalud.model.Usuario
+import  com.example.vidasalud.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+
 
 class AuthRepository {
     private val auth = FirebaseAuth.getInstance()
     private val db = FirebaseFirestore.getInstance()
 
-    suspend fun login(correo: String, clave: String): Usuario? {
+    suspend fun login(correo: String, clave: String): User? {
         return try {
-            //Intentar autenticar con Authentication de Firebase - admin
-            val resultado = auth.signInWithEmailAndPassword(correo, clave).await()
-            val usuario = resultado.user
-            if (usuario != null ) {
-                getUserFromFirestore(usuario.uid, correo) ?: Usuario (
-                    correo = correo,
-                    nombre = if (correo == "admin@vidasalud.cl") "Administrador" else "Usuario",
-                    rol = if (correo == "admin1@vidasalud.cl") "admin" else "cliente"
-                )
-            } else null
-        }catch (e: Exception) {
-            //Si falla Auth, intentar con Firestore
-            loginWithFirestore(correo, clave)
-        }
-    }
-
-    private suspend fun getUserFromFirestore(uid: String, correo: String): Usuario? {
-        return try {
-            val documento = db.collection("usuario").document(uid).get().await()
-            if (documento.exists()) {
-                Usuario(
-                    correo = documento.getString("correo") ?: correo,
-                    nombre = documento.getString("nombre") ?: "Usuario",
-                    rol = documento.getString("rol") ?: "cliente"
-                )
-            } else null
-        } catch (e: Exception) {
+            //Intentar autenticar con auth
+            when {
+                correo == "admin@nombrecaso.cl" -> {
+                    //Autenticación con Firebase Auth
+                    val resultado = auth.signInWithEmailAndPassword(correo, clave).await()
+                    User(
+                        correo = correo,
+                        nombre = "Administrador",
+                        rol = "admin"
+                    )
+                }
+                else -> {
+                    //Autenticación con la colección usuario de Firestore
+                    loginWithFirestore(correo,clave)
+                }
+            }
+        } catch (e: Exception){
             null
         }
     }
 
-    private suspend fun loginWithFirestore(correo: String, clave: String): Usuario? {
+
+
+    private suspend fun loginWithFirestore(correo: String, clave: String): User? {
         return try {
             val query = db.collection("usuario")
-                .whereEqualTo("correo",correo)
+                .whereEqualTo("correo", correo)
                 .whereEqualTo("clave", clave)
                 .get()
                 .await()
 
             if (!query.isEmpty) {
                 val doc = query.documents[0]
-                Usuario (
+                User(
                     correo = doc.getString("correo") ?: "",
+                    clave = doc.getString("clave") ?: "",
                     nombre = doc.getString("nombre") ?: "Cliente",
                     rol = doc.getString("rol") ?: "cliente"
                 )
