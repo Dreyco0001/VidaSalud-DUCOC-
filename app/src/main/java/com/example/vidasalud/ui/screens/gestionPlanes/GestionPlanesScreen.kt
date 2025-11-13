bdde584d36e862227e51d384d35276a4cf7a8289
Benjamin Rosales <144568456+benjarxv@users.noreply.github.com>
2025-11-13 12:34:09 -0300

    Crud pt2

---
+++ app/src/main/java/com/example/vidasalud/ui/screens/gestionPlanes/GestionPlanesScreen.kt
@@ -0,0 +1,14 @@
+package com.example.vidasalud.ui.screens.gestionPlanes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GestionPlanesScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Gestion de Planes")
+    }
+}
